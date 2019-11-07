05b3502e1fec3da1a875febf14cf888ee7fa5545
jessicaespinoza12 <43005006+jessicaespinoza12@users.noreply.github.com>
2019-11-06 18:27:26 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,6 +10,20 @@ $('document').ready(function() {
   // Write your code here!
 $("button").click(function() {
 var name= $("#input").val();
-$("#userChoice").append(name);
+$("#userChoice").text(name);
+ let Computerchoice = Math.random();
+    console.log(Computerchoice);
+    
+    if (Computerchoice > .80) {
+        $("#result").html("scissors");
+    }
+    else if (Computerchoice > .60) {
+      $("#result").html("paper");
+    }
+    else {
+      $("#result").html("rock");
+
+    }
 })
 });
+ 
